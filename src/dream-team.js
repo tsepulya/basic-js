d1079d7e38ed5a96121ad03268a9a87fbb083732
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 14:00:05 +0300

    fix: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,12 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (members === undefined || members === []) {
-    return null;
-  } else if (typeof (members) !== 'string' && typeof (members) !== 'object') {
+  if (Array.isArray(members) === false) {
     return false;
   } else {
     members = members.filter(elem => typeof (elem) === 'string');
-    return members.map(elem => elem[0].toUpperCase()).sort().join('');
+    return members.map(elem => elem.trimStart()[0].toUpperCase()).sort().join('');
   }
 }
