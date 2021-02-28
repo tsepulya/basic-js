efb58f5bc2d00fd2c85a2a7c58c07da9a7c88162
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 10:05:28 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (members === undefined || members === []) {
+    return null;
+  } else if (typeof (members) !== 'string' && typeof (members) !== 'object') {
+    return false;
+  } else {
+    members = members.filter(elem => typeof (elem) === 'string');
+    return members.map(elem => elem[0].toUpperCase()).sort().join('');
+  }
+}
