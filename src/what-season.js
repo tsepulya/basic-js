0931ec89d50bef2688fd201b0925e1ed003d1f4f
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 13:42:39 +0300

    fix: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -4,9 +4,9 @@ module.exports = function getSeason(date) {
   if (date === undefined) {
     return 'Unable to determine the time of year!';
   } else if (typeof (date.getMonth) !== 'function') {
-    return 'THROWN'
+    throw new Error('Unable to determine the time of year!');
   } else {
-    let month = date.getMonth();
-    return (month < 3 || month === 12) ? 'winter' : (month < 6) ? 'spring' : (month < 9) ? 'summer' : 'autumn';
+    let month = Date.prototype.getMonth.call(date);
+    return (month < 2 || month === 11) ? 'winter' : (month < 5) ? 'spring' : (month < 8) ? 'summer' : 'autumn';
   }
 }
