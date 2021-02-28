f80cd861d4a6a623ccef0a3f58e3fc357577e10f
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 09:59:43 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let sum = 0;
+  if (matrix !== undefined) {
+    for (let i = 0; i < matrix.length; i++) {
+      sum += (matrix[i].filter(elem => elem ==='^^')).length;
+    }
+  }
+  return sum;
+} 
