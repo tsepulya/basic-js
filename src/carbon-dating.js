b740c3a35a79d03729f8664e0d9ff67f7b7088cc
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 09:53:39 +0300

    feat: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string') {
+    return false;
+  } else {
+    sampleActivity = +sampleActivity;
+    if (sampleActivity > 15 || sampleActivity <= 0 || isNaN(sampleActivity) === true) {
+      return false;
+    } else {
+      return Math.ceil(Math.log(15 / sampleActivity) * 5730 / 0.693);
+    }
+  }
+}
