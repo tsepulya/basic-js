1b69d8a8579412320e833d5a4346dd61f9328c52
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 10:03:48 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  } else if (typeof (date.getMonth) !== 'function') {
+    return 'THROWN'
+  } else {
+    let month = date.getMonth();
+    return (month < 3 || month === 12) ? 'winter' : (month < 6) ? 'spring' : (month < 9) ? 'summer' : 'autumn';
+  }
+}
