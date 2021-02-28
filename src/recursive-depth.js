ef3c42dd62570670fef642b406376a101176244f
tsepulya <tsepeleva_anna@mail.ru>
2021-02-28 09:49:58 +0300

    feat: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    if (Array.isArray(arr) === false) {
+      return 0;
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) === true) {
+          count = Math.max(count, this.calculateDepth(arr[i]) + 1);
+        }
+      }
+      return count;
+    }
   }
-};
+}
