f932dd8fb79c86481a149c78eb84fa4136d57d05
tsepulya <tsepeleva_anna@mail.ru>
2021-03-07 06:23:05 +0300

    fix: simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,31 +3,35 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
   chain: [],
 
+  getLength() {
+    return this.chain.length;
+  },
+
   addLink(value) {
     (value !== undefined) ? this.chain.push(`( ${value} )`): this.chain.push(`( )`);
     return this;
   },
-  getLength() {
-    return this.chain.length;
-  },
+
   removeLink(position) {
     if (typeof position === "number" && position > 0 && position <= this.chain.length) {
       this.chain.splice(position - 1, 1);
       return this;
     } else {
-      chain = [];
+      this.chain = [];
       throw Error();
     }
   },
+
   reverseChain() {
     this.chain.reverse();
     return this;
   },
+
   finishChain() {
     let result = this.chain.join('~~');
-    chain = [];
+    this.chain = [];
     return result;
-  }
+  },
 };
 
 module.exports = chainMaker;
