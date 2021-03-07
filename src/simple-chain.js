9c0088072c593b1d18239763e92cb93b9b9ca0ab
tsepulya <tsepeleva_anna@mail.ru>
2021-03-07 06:06:39 +0300

    feat: simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+  chain: [],
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    (value !== undefined) ? this.chain.push(`( ${value} )`): this.chain.push(`( )`);
+    return this;
+  },
+  getLength() {
+    return this.chain.length;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position === "number" && position > 0 && position <= this.chain.length) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      chain = [];
+      throw Error();
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    chain = [];
+    return result;
   }
 };
 
