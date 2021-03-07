04e69f736f53d2c14c4ab05336f2cdb3c812388a
tsepulya <tsepeleva_anna@mail.ru>
2021-03-07 14:45:40 +0300

    feat: start vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (name) {
+    this.name = name;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw Error();
+    }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw Error();
+    }
+
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+//npm run test ./test/vigenere-cipher.test.js
