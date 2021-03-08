61ede635a0498159a76f7ddd3e6e328511ddcf81
tsepulya <tsepeleva_anna@mail.ru>
2021-03-08 07:44:59 +0300

    fix: end transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,33 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if(!Array.isArray(arr)) {
+  if(!Array.isArray(arr)) {   // проходим ошибку, если параметр - не массив
     throw Error();
-  } else {
+  } else { 
     let array2 = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (typeof (arr[i]) === 'number') {
-        array2.push(arr[i]);
-      } else if (arr[i] === '--double-prev') {
-        if (arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') {
-          array2.push(arr[i - 1]);
-        }
-      } else if (arr[i] === '--double-next') {
-        if (arr[i + 1] !== undefined) {
+    for (let i = 0; i < arr.length; i++) { // проходим по всей длине массива
+      if (arr[i] === '--double-prev') { // если элемент double-prev => 
+        if (i > 0) { // => если предыдущий не undefined  
+          array2.push(array2[array2.length - 1]);
+        }  // => добавляем предыдущий элемент из нового массива!
+      } else if (arr[i] === '--double-next') { // если элемент double-next и следующий не undefined => добавляем следующий
+        if (i < arr.length - 1) {
           array2.push(arr[i + 1]);
         }
-      } else if (arr[i] === '--discard-next') {
-        if (arr[i + 1] !== undefined && arr[i + 2] !== '--discard-prev') {
-          i++;
+      } else if (arr[i] === '--discard-next') { // если элемент discard-next =>
+        if (i < arr.length) { // => если следующий не undefined =>
+          array2.push(undefined)
+          i++; // перескакиваем через следующий элемент
         }
-      } else if (arr[i] === '--discard-prev') {
-        if (arr[i - 1] !== undefined) {
+      } else if (arr[i] === '--discard-prev') { // если элемент discard-prev и предыд.элемент не undefined => удалить последний элемент в массиве
+        if (i > 0) {
           array2.pop();
-
         }
+      } else { // если элемент - прочее => добавляем в новый массив
+        array2.push(arr[i]);
       }
-
-    }
-    return array2;
+  }
+    let arrayEnd = array2.filter(elem => elem !== undefined)
+    return arrayEnd;
   }
 }
+
+
+// npm run test ./test/transform-array.test.js
